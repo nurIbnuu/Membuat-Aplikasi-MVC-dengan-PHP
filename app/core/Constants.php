de177e5eafa4e0be8c5f86fa52ffc5fe406eddf4
Muhammad Nur Ibnu Hubab <nuribnuu@gmail.com>
2024-09-06 14:02:29 +0700

    Membuat Aplikasi MVC dengan PHP #6 Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/Membuat_Aplikasi_MVC_dengan_PHP/public');
---
+++ app/views/home/index.php
@@ -0,0 +1,3 @@
+  <div class="container mt-4">
+    <h1 class="ms-4">Selamat Datang di Website Saya</h1>
+  </div>
