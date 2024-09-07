964731303a5ce7b89ad5a73d0c40bccee8b8cf0c
Muhammad Nur Ibnu Hubab <nuribnuu@gmail.com>
2024-09-07 14:26:52 +0700

    Membuat Aplikasi MVC dengan PHP #7 Model

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,55 @@
+<?php
+
+class Mahasiswa_model
+{
+  // private $mhs =
+  // [
+  //   [
+  //     "nama" => 'Sandhika Galih',
+  //     "nrp" => '043040023',
+  //     "email" => 'sandhikagalih@unpas.ac.id',
+  //     "jurusan" => 'Teknik Informatika',
+  //   ],
+  //   [
+  //     "nama" => 'Doddy Ferdiansyah',
+  //     "nrp" => '123456789',
+  //     "email" => 'doddy@gmail.com',
+  //     "jurusan" => 'Teknik Mesin',
+  //   ],
+  //   [
+  //     "nama" => 'Erik',
+  //     "nrp" => '123456789',
+  //     "email" => 'erik@gmail.com',
+  //     "jurusan" => 'Teknik Industri',
+  //   ]
+  // ];
+
+  private $dbh; # database handler
+  private $stmt;
+
+  
+  public function __construct()
+  {
+    # data source name
+    $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+    try
+    {
+      $this->dbh = new PDO($dsn, 'root', '');
+    }
+    catch(PDOException $e)
+    {
+      die($e->getMessage());
+    }
+  }
+
+  public function getAllMahasiswa()
+  {
+    // return $this->mhs;
+
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+    $this->stmt->execute();
+
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,5 @@
   <div class="container mt-4">
     <h1 class="ms-4">Selamat Datang di Website Saya</h1>
+    <hr>
+    <p class="ms-5">Halo, nama saya <?= $data['nama']; ?></p>
   </div>
