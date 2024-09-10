be3449049d66446f955c4ea9324eb61a6bee59ae
nurIbnuu <167744992+nurIbnuu@users.noreply.github.com>
2024-09-10 14:46:44 +0700

    Membuat Aplikasi MVC dengan PHP #9 Insert Data

--- pertemuan-6-dst/app/views/mahasiswa/index.php
+++ pertemuan-6-dst/app/views/mahasiswa/index.php
@@ -1,7 +1,11 @@
 <div class="container">
   <div class="row">
     <div class="col-6 mt-4 ms-4">
-      <h3>Daftar Mahasiswa</h3>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+          Tambah Data Mahasiswa
+      </button>
+
+      <h3 class="mt-2">Daftar Mahasiswa</h3>
       <ul class="list-group">
         <?php foreach($data['mhs'] as $mhs) : ?>
           <li class="list-group-item">
@@ -16,6 +20,58 @@
           </li>
         <?php endforeach; ?>
       </ul>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+
+          <div class="mb-3 row">
+            <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+          </div>
+          
+          <div class="mb-3 row">
+            <label for="nrp" class="col-sm-2 col-form-label">NRP</label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="nrp" name="nrp">
+            </div>
+          </div>
+
+          <div class="mb-3 row">
+            <label for="email" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="email" name="email">
+            </div>
+          </div>
+          
+          <div class="mb-3 row">
+            <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+            <div class="col-sm-10">
+              <select class="form-select" aria-label="Default select example" id="jurusan" name="jurusan">
+                <option value="Teknik Informatika">Teknik Informatika</option>
+                <option value="Teknik Mesin">Teknik Mesin</option>
+                <option value="Teknik Pangan">Teknik Pangan</option>
+              </select>
+            </div>
+          </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
     </div>
   </div>
 </div>
